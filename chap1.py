6fa900615ac9a0d007da9a82e1acb236379f3ca5
bambook <bambook729@gmail.com>
2017-01-12 23:40:36 +0900

    [update] chap1 No.07

--- chap1.py
+++ chap1.py
@@ -106,5 +106,15 @@ def no06():
     print('"se" within Y :', "se" in Y)
 
 
+# 07. テンプレートによる文生成
+# 引数x, y, zを受け取り「x時のyはz」という文字列を返す関数を実装せよ．
+# さらに，x=12, y="気温", z=22.4として，実行結果を確認せよ．
+def template(x, y, z):
+    return "{}時の{}は{}".format(x, y, z)
+
+def no07():
+    print(template(x=12, y="気温", z=22.4))
+
+
 if __name__ == '__main__':
-    no06()
+    no07()
