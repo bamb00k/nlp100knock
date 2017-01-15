44835dc4953fe068e17f8f882c956d57d142caeb
bambook <bambook729@gmail.com>
2017-01-16 00:57:05 +0900

    [update] chap1 No.09

--- chap1.py
+++ chap1.py
@@ -135,5 +135,27 @@ def no08():
     print(decode)
 
 
+# 09. Typoglycemia
+# スペースで区切られた単語列に対して，各単語の先頭と末尾の文字は残し，
+# それ以外の文字の順序をランダムに並び替えるプログラムを作成せよ．
+# ただし，長さが４以下の単語は並び替えないこととする．
+# 適当な英語の文（例えば"I couldn't believe that I could actually understand what I was reading :
+#  the phenomenal power of the human mind ."）を与え，その実行結果を確認せよ．
+def no09():
+    import random
+
+    text = "I couldn't believe that I could actually understand what I was reading : " \
+           "the phenomenal power of the human mind ."
+    text = text.split()
+    ret = []
+    for word in text:
+        if len(word) > 4:
+            mid_w = list(word[1:-1])
+            random.shuffle(mid_w)
+            word = word[0] + ''.join(mid_w) + word[-1]
+        ret.append(word)
+    print(' '.join(ret))
+
+
 if __name__ == '__main__':
-    no08()
+    no09()
