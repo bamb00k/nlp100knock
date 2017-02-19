ca71dff93e8afd41e7f3c0790dbe0d573bc694f3
bambook <bambook729@gmail.com>
2017-02-19 23:46:05 +0900

    [update] chap2 No.12

--- chap2.py
+++ chap2.py
@@ -37,5 +37,28 @@ def no11():
     # expand -t 1 hightemp.txt
 
 
+# 12. 1列目をcol1.txtに，2列目をcol2.txtに保存
+# 各行の1列目だけを抜き出したものをcol1.txtに，2列目だけを抜き出したものをcol2.txtとしてファイルに保存せよ．
+# 確認にはcutコマンドを用いよ．
+def no12():
+    import pandas as pd
+
+    df = pd.read_csv(fname, '\t', header=None)
+    df[0].to_csv('col1.txt', index=False)
+    df[1].to_csv('col2.txt', index=False)
+
+    # Linuxコマンド
+    # $ cut -f 1 hightemp.txt > col1.txt
+    # $ cut -f 2 hightemp.txt > col2.txt
+
+    # pandas を使わないパターン
+    # with open(fname, 'r') as f:
+    #     reader = csv.reader(f)
+    #     with open('col1.txt', 'w') as f_col1, open('col2.txt', 'w') as f_col2:
+    #         for row in reader:
+    #             f_col1.write(row[0])
+    #             f_col2.write(row[1])
+
+
 if __name__ == '__main__':
-    no11()
+    no12()
